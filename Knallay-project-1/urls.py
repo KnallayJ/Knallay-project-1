6e39f85bc5e690df0ce6a7c1fff0e483fc0a8d75
KnallayJ <64495164+KnallayJ@users.noreply.github.com>
2020-04-29 22:46:14 -0700

    Things added
    Archive, Comments, Trip Number editing added

--- Knallay-project-1/urls.py
+++ Knallay-project-1/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
 
-path('/contract/<int:contract_id>',views.contract, name='contract_detail'),
-        # Josh contract render page 
-    path('/edit_contract/<int:contract_id>',views.edit_contract),
-        # Josh contract edit route
+urlpatterns = [
+    path("/contract/<int:contract_id>", views.contract, name="contract_detail"),
+            # GET -> render contract.html
+
+        path('/edit_contract/<int:contract_id>',views.edit_contract),
+            # Josh contract edit route
+        path('/archive_contract/<int:contract_id>',views.archive_contract),
+        path('/contract_comment/<int:contract_id>',views.contract_comment),
+]
